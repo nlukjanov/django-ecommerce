452c44544b2a330aa55073a38abe88e48bf9e2ec
Nikita Lukjanov <nikita.lukjanov@nandos.co.uk>
2020-11-29 08:23:11 +0000

    feat: add pages views

--- store/views.py
+++ store/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def store(request):
+    """
+    store view
+    """
+    context = {}
+    return render(request, 'store/store.html', context)
+
+def cart(request):
+    """
+    store view
+    """
+    context = {}
+    return render(request, 'store/cart.html', context)
+
+def checkout(request):
+    """
+    store view
+    """
+    context = {}
+    return render(request, 'store/checkout.html', context)
