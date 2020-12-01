d1634f3120a1012210319b448c94163546209b20
Nikita Lukjanov <nikita.lukjanov@nandos.co.uk>
2020-12-01 19:41:14 +0000

    feat: add blank image url error handling

--- store/models.py
+++ store/models.py
@@ -27,6 +27,17 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+    @property
+    def imageURL(self):
+        """
+        image url error handling via property
+        """
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
+
 
 class Order(models.Model):
     """
