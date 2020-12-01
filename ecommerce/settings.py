f464a878ebf297ea631a0c3c424cec892f2b3156
Nikita Lukjanov <nikita.lukjanov@nandos.co.uk>
2020-12-01 08:34:25 +0000

    feat: add images to products

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images/products')
+
+MEDIA_URL = '/images/products/'
