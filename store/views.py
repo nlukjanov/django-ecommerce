d0f7f25b8ced87e72dcc720189516af7eaac3211
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-12-05 12:31:55 +0000

    feat: display data from db in checkout

--- store/views.py
+++ store/views.py
@@ -1,7 +1,7 @@
 # pylint: disable=no-member
 from django.shortcuts import render
 
-from .models import *
+from .models import Order, Product
 
 
 def store(request):
@@ -34,5 +34,14 @@ def checkout(request):
     """
     checkout view
     """
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(
+            customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        # empty cart for non logged in users
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        items = []
+    context = {'items': items, 'order': order}
     return render(request, 'store/checkout.html', context)
