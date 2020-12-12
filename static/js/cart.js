eec8b3ff48f9075b011037899d52285ce6a6be60
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-12-12 10:18:51 +0000

    refactor: cart js

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,18 +1,20 @@
 console.log('hello');
 const updateBtns = document.querySelectorAll('.update-cart');
 for (let index = 0; index < updateBtns.length; index++) {
-  updateBtns[index].addEventListener('click', function () {
-    const productId = this.dataset.product;
-    const action = this.dataset.action;
-    console.log('productId', productId);
-    console.log('action', action);
+  updateBtns[index].addEventListener('click', handleClick);
+}
 
-    console.log('user', user);
-    if (user === 'AnonymousUser') {
-      console.log('User Not logged in');
-    }
-    updateUserOrder(productId, action);
-  });
+function handleClick() {
+  const productId = this.dataset.product;
+  const action = this.dataset.action;
+  console.log('productId', productId);
+  console.log('action', action);
+
+  console.log('user', user);
+  if (user === 'AnonymousUser') {
+    console.log('User Not logged in');
+  }
+  updateUserOrder(productId, action);
 }
 
 const updateUserOrder = (productId, action) => {
