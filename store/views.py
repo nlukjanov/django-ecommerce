063da3cd1572e3fe2d929aa41aaced3cedb11bba
Nikita Lukjanov <nikita.lukjanov@nandos.co.uk>
2022-04-08 08:15:36 +0100

    feat: (WIP) cart view

--- store/views.py
+++ store/views.py
@@ -16,7 +16,14 @@ def cart(request):
     """
     cart view
     """
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(
+            customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+    context = {'items': items}
     return render(request, 'store/cart.html', context)
 
 
